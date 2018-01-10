5584e9912130fbf5a44b87a1094d9486d6610408
Priyesh Kumar <priyeshkumar16@gmail.com>
2018-01-10 15:06:12 +0530

    just changed indentation

--- Solutions/1_16priyesh.cpp
+++ Solutions/1_16priyesh.cpp
@@ -8,8 +8,8 @@ int main(){
 	while(t--){
 		int x,y,p;
 		cin>>x>>y>>p;
-    	if(p%2!=0) x*=2;
-	    int result=(x>y)?(x/y):(y/x);
+    		if(p%2!=0) x*=2;
+	   	int result=(x>y)?(x/y):(y/x);
 		cout<<result<<endl;
 	}
 	return 0;
