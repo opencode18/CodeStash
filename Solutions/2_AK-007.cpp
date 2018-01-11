e1e9bd5668e431dcf720374e81319132e613a88a
Ayush <ayushkgupta007@gmail.com>
2018-01-11 14:26:54 +0530

    Changes made

--- Solutions/2_AK-007.cpp
+++ Solutions/2_AK-007.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int n,i,j;
-    cin>>n;
-    int a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-     
-    
+    int i,j,m;
+   vector <int> a;
+    while(cin>>m)
+    {
+      a.push_back(m);
+    }
+    int n=a.size();
     float ans[n]={0};
     float c=0,d=0;
     for (i=0;i<n;i++)
