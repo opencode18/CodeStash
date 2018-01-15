308fb8e6c298e59f713adf44709a1841405c4577
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-01-16 02:12:59 +0530

    Update 7_GOVINDDIXIT.py

--- Solutions/GOVINDDIXIT/7_GOVINDDIXIT.py
+++ Solutions/GOVINDDIXIT/7_GOVINDDIXIT.py
@@ -1,4 +1,17 @@
 t=int(input())
 for i in range(t):
+    word=""
+    ans=""
     a=str(input())
-    print(a[::-1])
+    for j in a:
+        if j=='.':
+            if ans=="":
+                ans=ans+word
+            else:
+                ans=word+'.'+ans
+            word=""
+        else:
+            word=word+j
+    if word!="":
+        ans=word+'.'+ans
+    print(ans)
