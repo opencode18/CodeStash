cc1f2cdf01eaa7727b2880b20a786fd5e56b6b7a
Anshu-ros <anshuyadavros@gmail.com>
2018-01-19 03:51:14 +0530

    Added Day_10 solution

---
+++ Solutions/8_Anshu-ros.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n],i;
+        for(i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int k;
+        cin>>k;
+        int ans;
+        ans=longarr(a[],n,k);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+int longarr (int a[],int n,int k)
+{
+    unordered_map<int, int> ht;
+    int s=0,len=0;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+        if(s==k)
+        {
+            len=i+1;
+        }
+        else if (ht.find(s)==ht.end())
+        {
+            ht[s]=i;
+        }
+        if(ht.find(s-k)!=ht.end())
+        {
+            if(len<(i-ht[s-k]))
+                len=i-ht[s-k];
+        }
+    }
+    return len;
+}
