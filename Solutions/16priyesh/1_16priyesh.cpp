30b61cd3d95baa4ca47f196fe6c3425a1bc78b78
ritwik1503 <35749253+ritwik1503@users.noreply.github.com>
2020-10-02 05:01:16 +0530

    Update 1_16priyesh.cpp

--- Solutions/16priyesh/1_16priyesh.cpp
+++ Solutions/16priyesh/1_16priyesh.cpp
@@ -1,14 +1,18 @@
 //coded by 16priyesh
 #include <bits/stdc++.h>
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
 int main(){ 
+	//To reduce the runtime
+	FAST;
 	int t;
 	cin>>t;
 	while(t--){
 		int x,y,p;
 		cin>>x>>y>>p;
-    		if(p%2!=0) x*=2;
+		//Check odd bitwise.
+    		if(p&1) x*=2;
 	   	int result=(x>y)?(x/y):(y/x);
 		cout<<result<<endl;
 	}
